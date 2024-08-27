722d16e3f80d65439204202fe35c217a4c721d08
yuenkaden <kadeny2027@student.cis.edu.hk>
2024-08-27 14:32:35 +0800

    quiz 1.1 completed

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,27 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Kaden
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your age: ");
+        int age = scanner.nextInt();
+        System.out.println("Enter your first name: ");
+        String firstName = scanner.next();
+        System.out.println("Enter your favourite food: ");
+        String favouriteFood = scanner.next();
+        scanner.close();
+        System.out.println("First name: " + firstName);
+        System.out.println("Your age : " + age);
+        System.out.println("Favourite food : " + favouriteFood);
+
+
+
 
 
 
